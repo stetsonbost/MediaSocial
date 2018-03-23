b438e1f09a5f1136fa3d355ef86a489d2dc4210d
hmc-cs-mmelaku <mmelaku@cs.hmc.edu>
2018-03-23 15:45:18 -0700

    problems with displaying book list

--- MediaSocial/catalog/views.py
+++ MediaSocial/catalog/views.py
@@ -22,9 +22,12 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    context_object_name = 'my_book_list'   # your own name for the list as a template variable
-    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
-    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains=' ')[:5] # Get 5 books containing the title war
+    template_name = 'catalog/book_list.html'  # Specify your own template name/location
 
     def get_queryset(self):
         return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+class BookDetailView(generic.DetailView):
+    model = Book
