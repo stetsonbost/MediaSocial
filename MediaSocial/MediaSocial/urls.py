5fcd8ab2ad47bfbc85825b2163d23a28da0cf905
Marko Mandic <Marko@Duras-MacBook-Pro.local>
2018-04-06 14:43:37 -0700

    user login created

--- MediaSocial/MediaSocial/urls.py
+++ MediaSocial/MediaSocial/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/')),
-    # path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#Add Django site authentication urls (for login, logout, password management)
